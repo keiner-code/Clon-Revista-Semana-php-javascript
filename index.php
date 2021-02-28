60884f3e942c05c462ce88edd24df109c8640b4b
keiner peñaranda <keinerjpp@gmail.com>
2021-02-27 20:02:22 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo/estilo.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+        <div class="row" id="header" >
+
+            <div class="col-md-3">
+            <div class="logo"><h1>Semana</h1></div>
+            </div>
+
+            <div class="col-md-6">
+            <p class="fecha">Sábado, 27 de abril de 2019 | <a href="">Newsleetter |</a></p>
+            </div>
+
+            <div class="col-md-3" id="btnheadr">
+              <div class="btniniciar"><button type="button" class="btn btn-outline-dark">Iniciar Session</button></div>
+              <div class="btnpalo"><p> | </p></div>
+              <div class="btnsub"><button type="button" class="btn btn-outline-danger">Subcribase</button></div>
+              <div class="btnpalo"><p> | </p></div>
+              <div class="buscar">
+                  <input type="search" name="Buscar" class="bus" id="Buscar">
+
+                  <div class="icono"> 
+                      <a href="" id="ico"> <i class="fas fa-search"></i></a>
+                  </div>
+              </div>
+            </div>  
+        </div>
+
+        <div class="row" id="Menuvar">
+          <div class="col-md-12">
+             <nav>
+                 <ul class="nav">
+                     <li class="nav-item"> <a class="nav-link active" href="http://">NACION |</a></li>
+                     <li class="nav-item"> <a class="nav-link active" href="http://">OPINION |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">ECONOMIA |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">VIDA MODERNA |</a></li>  
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">GENTE |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">CULTURA |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">MUNDO |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">TECNOLOGIA |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">EDUCACION |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">DEPORTE |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">SOSTENIBILIDAD |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">FORO SEMANA |</a></li>
+                     <li class="nav-item"> <a class="nav-link activa" href="http://">EDICION EMPRESA |</a></li>
+                     <li class="nav-item"> <a class="fas fa-play" id="play" href="http://">  |</a></li>
+                     <li class="nav-item"> <a class="fas fa-camera" id="play" href="http://">  |</a></li>
+                     <li class="nav-item"> <a class="fas fa-bars" id="play" href="http://">  |</a></li>
+
+                 </ul>
+             </nav>
+          </div>
+        </div>
+        <hr class="linea">
+
+        <div class="container">
+           <div class="row" id="uso">
+               <div class="col-md-10">
+                   <p class="tendencia">TENDENCIA </p>
+                   <p class="fa fa-angle-right" id="otroicono"></p>
+                   <nav class="subnav">
+                       <ul class="nav" id="nav">
+                           <li class="nav-item"><a class="nav-link activa" href="http://">ESTADOS UNIDOS QUITÓ VISA</a></li>
+                           <li class="nav-item"><a class="nav-link activa" href="http://">EXPLOSIÓN EN AVENIDA ROJAS</a></li>
+                           <li class="nav-item"><a class="nav-link activa" href="http://">MEGHAN MARKLE MATERNIDAD</a></li>
+                           <li class="nav-item"><a class="nav-link activa" href="http://">CHAT RAFAEL URIBE NOGUERA HERMANOS</a></li>
+                       </ul>
+                   </nav>
+               </div>   
+                <div class="col-md-2" id="mas">
+                <li class="nav-item"><a class="nav-link activa"href="http://">VER MAS</a></li>
+                </div>
+           </div>
+           <hr class="linea1">
+
+           <div class="row">
+             <!--img src="img//img.png" alt="" srcset="" class="img"-->
+
+           </div>
+        </div>
+        
+        <script src="javascript/script.js"></script>
+</body>
+</html>
---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+document.getElementById("ico").addEventListener("mouseover",function(){
+    var x = document.getElementById('Buscar');
+    x.style.display="block";
+    x.style.cssFloat="left";
+
+});
+/*document.getElementById("ico").addEventListener("change",function(){
+    document.getElementById('Buscar').style.display="none";
+});*/
+
+
+
+
+/*function Mostrar(){
+    document.getElementById('Buscar').style.display="block";
+    return true;
+}
+function Quitar(){
+    document.getElementById('Buscar').style.display="none";
+}
+
+document.getElementById('ico').addEventListener('mouseover',Mostrar);
+//document.getElementById('ico').addEventListener('mouseout',Quitar);*/
+
